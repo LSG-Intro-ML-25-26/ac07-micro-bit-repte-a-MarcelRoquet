cb1dd1d9473e84a2b3bfaf4ecac95ad63f8deccd
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2025-12-09 22:23:35 +0100

    Actualització main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
-hand = 0
-
-def on_gesture_shake():
+def on_button_pressed_a():
     global hand
-    hand = randint(1, 3)
-    if hand == 0:
+    hand = hand + 1
+    if hand > 3:
+        hand = 1
+    if hand == 1:
         basic.show_icon(IconNames.SMALL_SQUARE)
-        music.play(music.string_playable("- - - - - - - - ", 120),
-            music.PlaybackMode.UNTIL_DONE)
     elif hand == 2:
         basic.show_icon(IconNames.SQUARE)
     else:
         basic.show_icon(IconNames.SCISSORS)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_shake():
+    global handCPU
+    handCPU = randint(1, 3)
+    if handCPU == 1:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif handCPU == 2:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_b():
+    global handCPU
+    handCPU = randint(1, 3)
+    basic.show_string("CPU")
+    if handCPU == 1:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif handCPU == 2:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+    basic.show_string("Tu")
+    if hand == 1:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif hand == 2:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+    if hand == 1:
+        if handCPU == 2:
+            basic.show_string("LOSE")
+        elif handCPU == 3:
+            basic.show_string("Guanyes")
+        else:
+            basic.show_string("empat")
+    elif hand == 2:
+        if handCPU == 1:
+            basic.show_string("gunayes")
+        elif handCPU == 3:
+            basic.show_string("perds")
+        else:
+            basic.show_string("empat")
+    elif handCPU == 1:
+        basic.show_string("perds")
+    elif handCPU == 2:
+        basic.show_string("guanyes")
+    else:
+        basic.show_string("empat")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+handCPU = 0
+hand = 0
+hand = 0
+basic.show_string("Inici")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,14 +1,72 @@
-let hand = 0
-input.onGesture(Gesture.Shake, function () {
-    hand = randint(1, 3)
+input.onButtonPressed(Button.A, function () {
+    hand = hand + 1
+    if (hand > 3) {
+        hand = 1
+    }
     if (hand == 1) {
         basic.showIcon(IconNames.SmallSquare)
-        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     } else if (hand == 2) {
         basic.showIcon(IconNames.Square)
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.Scissors)
-        music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    handCPU = randint(1, 3)
+    if (handCPU == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (handCPU == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    handCPU = randint(1, 3)
+    basic.showString("CPU")
+    if (handCPU == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (handCPU == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+    basic.showString("Tu")
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+    if (hand == 1) {
+        if (handCPU == 2) {
+            basic.showString("LOSE")
+        } else if (handCPU == 3) {
+            basic.showString("Guanyes")
+        } else {
+            basic.showString("empat")
+        }
+    } else if (hand == 2) {
+        if (handCPU == 1) {
+            basic.showString("gunayes")
+        } else if (handCPU == 3) {
+            basic.showString("perds")
+        } else {
+            basic.showString("empat")
+        }
+    } else if (handCPU == 1) {
+        basic.showString("perds")
+    } else if (handCPU == 2) {
+        basic.showString("guanyes")
+    } else {
+        basic.showString("empat")
+    }
+})
+let handCPU = 0
+let hand = 0
+hand = 0
+basic.showString("Inici")
+basic.forever(function () {
+	
 })
